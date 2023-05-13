1a8a17bdeb8e7c0eba0880f9174a94143b216caf
raul.albin <raul200794@gmail.com>
2023-05-13 18:54:00 +0200

    update pattern matching

--- bin/pattern_matching.dart
+++ bin/pattern_matching.dart
@@ -1,25 +1,21 @@
-
-
-
-
-
-
-
-(String, int) getNombreEdad(){
-  return ('DevCiencia', 25);
-}
-
-//Pattern matching
-(String, String) userInfo(Map<String, dynamic> json) {
-  return (json['name'] as String, json['email'] as String);
-}
-
 void main(List<String> args) {
-  //record
-  final (nombre, edad) = getNombreEdad();
-  print('$nombre tiene $edad años');
+  (String, int, String) getNombreEdadEmail() {
+    return ('DevCiencia', 25, 'contacto@devciencia.com');
+  }
 
-  //pattern matching
-  final (usuario,correo) = userInfo({'name': 'DevCiencia', 'email':'contacto@devciencia.com'});
+  final (nombre, edad, email) = getNombreEdadEmail();
+  print('$nombre tiene $edad años y su email es $email');
+
+
+  //Pattern matching
+  (String, String) userInfo(Map<String, dynamic> json) {
+    return (json['name'] as String, json['email'] as String);
+  }
+
+  final (usuario, correo) =
+      userInfo({'name': 'DevCiencia', 'email': 'contacto@devciencia.com'});
   print('El correo de $usuario es $correo');
+
+
+
 }
